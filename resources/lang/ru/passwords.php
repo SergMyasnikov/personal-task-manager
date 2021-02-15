bf2ea2769a2604d3407c6330e271bf86f1a4f69e
msi <myasnikov.si@yandex.ru>
2021-02-15 07:37:18 +0300

    Auth localization

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Пароль был успешно сброшен!',
+    'sent' => 'Ссылка для сброса пароля направлена на указанный адрес!',
+    'throttled' => 'Подождите некоторое время прежде, чем повторить попытку.',
+    'token' => 'Неправильный токен для сброса пароля.',
+    'user' => "Пользователь с указанным адресом электронной почты не обнаружен.",
+
+];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="{{ asset('css/pagination.css') }}" rel="stylesheet">
     <link href="{{ asset('css/menu.css') }}" rel="stylesheet">
-    <title>Personal Task Manager</title>
+    <title>{{ config('app.name', 'Personal Task Manager') }}</title>
 </head>
 <body>
 	@include('inc.header')
